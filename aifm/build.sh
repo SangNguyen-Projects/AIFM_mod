a8ab3c0dff8574d5ae9921f27aed451f0b01568b
Sang Nguyen <sangnguyen@10-249-95-80.wireless.oregonstate.edu>
2026-07-10 13:15:12 -0700

    modified build scripts for just a single test

--- aifm/build.sh
+++ aifm/build.sh
@@ -2,13 +2,18 @@
 
 # AIFM Core.
 make clean
-make -j$(nproc) || { echo 'Failed to build AIFM Core.'; exit 1; }
+
+make -j$(nproc) bin/test_tcp_array_add || { echo 'Failed to build AIFM Core.'; exit 1; }
+
+echo 'Successfully built test_tcp_array_add.'
+
+#make -j$(nproc) || { echo 'Failed to build AIFM Core.'; exit 1; }
 
 # Far-Mem Snappy.
-cd snappy
-rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. || { echo 'Failed to build Snappy.'; exit 1; }
-make -j
-cd ..
+#cd snappy
+#rm -rf build
+#mkdir build
+#cd build
+#cmake -DCMAKE_BUILD_TYPE=Release .. || { echo 'Failed to build Snappy.'; exit 1; }
+#make -j
+#cd ..
